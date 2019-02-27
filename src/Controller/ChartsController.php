edc656a611c818f0b6495cc765e8f134e55cbd36
Josh Butler <josh.butler929@gmail.com>
2019-02-26 19:13:28 -0600

    Bug Fix: Empty data throws error
    When dataset is returned, some dates were missing from 30 days frame causing the chart to throw errors. The bug fix replaces missing dates with zero.

--- src/Controller/ChartsController.php
+++ src/Controller/ChartsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Nyholm\DSN;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,6 +20,7 @@ class ChartsController extends AbstractController
      * @var ParameterBagInterface
      */
     private $bag;
+    private $utc;
 
     public function __construct(ParameterBagInterface $bag)
     {
@@ -37,6 +39,7 @@ class ChartsController extends AbstractController
             dd($dsn);
         }
 
+        $this->utc = new \DateTimeZone('UTC');
     }
 
     /**
@@ -77,14 +80,14 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $sql = "select round(avg(total_earned)) as y, earned_on as x from v_squadron_daily_total where earned_on between now() - interval 30 day and now() group by earned_on";
         $tmp = $this->fetch_sql($sql, [$squadron_id]);
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $results['status'] = 200;
         $jsonResponse = new JsonResponse($results);
@@ -107,14 +110,14 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $sql = "select round(avg(total_earned)) as y, earned_on as x from v_squadron_type_total where earning_type_id='1' and earned_on between now() - interval 30 day and now() group by earned_on";
         $tmp = $this->fetch_sql($sql, [$squadron_id]);
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $results['status'] = 200;
         $jsonResponse = new JsonResponse($results);
@@ -137,14 +140,14 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $sql = "select round(avg(total_earned)) as y, earned_on as x from v_squadron_type_total where earning_type_id='4' and earned_on between now() - interval 30 day and now() group by earned_on";
         $tmp = $this->fetch_sql($sql, [$squadron_id]);
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $results['status'] = 200;
         $jsonResponse = new JsonResponse($results);
@@ -167,7 +170,7 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $sql = "select round(avg(total_earned)) as y, earned_on as x from v_squadron_type_total where earning_type_id='5' and earned_on between now() - interval 30 day and now() group by earned_on";
         $tmp = $this->fetch_sql($sql, [$squadron_id]);
@@ -180,8 +183,7 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'] + $buy[$row['x']];
         }
-
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $results['status'] = 200;
         $jsonResponse = new JsonResponse($results);
@@ -204,19 +206,34 @@ class ChartsController extends AbstractController
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $sql = "select round(avg(total_earned)) as y, earned_on as x from v_squadron_mission_total where earned_on between now() - interval 30 day and now() group by earned_on";
         $tmp = $this->fetch_sql($sql, [$squadron_id]);
         foreach ($tmp as $i => $row) {
             $data[$row['x']] = $row['y'];
         }
-        $results['data'][] = $data;
+        $results['data'][] = $this->fill_missing_dates($data, 30);
 
         $results['status'] = 200;
         $jsonResponse = new JsonResponse($results);
 
         return $jsonResponse;
+    }
+
+    private function fill_missing_dates($data, $num_days_ago)
+    {
+        $current_date = new \DateTime('now', $this->utc);
+        $one_day_interval = new \DateInterval('P1D');
+        $current_date->sub(new \DateInterval('P30D'));
+        $new_data = [];
+
+        for ($d = 0; $d < $num_days_ago; $d++) {
+            $date = $current_date->format('Y-m-d');
+            $new_data[$date] = is_null($data[$date]) ? 0 : $data[$date];
+            $current_date->add($one_day_interval);
+        }
+        return $new_data;
     }
 
     private function fetch_sql($sql, $params = null)
