5c6c023b0ff580529babee2a489d5b215b699993
Josh Butler <josh.butler929@gmail.com>
2019-09-14 21:54:48 -0400

    Add locked achievements

--- src/Controller/AchievementController.php
+++ src/Controller/AchievementController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\AchievementRuleRepository;
 use App\Service\CommanderDataHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ class AchievementController extends AbstractController
     /**
      * @Route("/achievement", name="achievement")
      */
-    public function index(CommanderDataHelper $dataHelper)
+    public function index(CommanderDataHelper $dataHelper, AchievementRuleRepository $achievementRuleRepository)
     {
         /**
          * @var User $user
@@ -22,12 +23,16 @@ class AchievementController extends AbstractController
         $dataHelper->checkForNewAchievements($user);
         $commander_data = $dataHelper->getData($user);
         $unlocked_achievements = $user->getAchievements()->toArray();
+        $locked_achievements = $achievementRuleRepository->findLockedAchievements($user->getId());
+
         dump($commander_data);
+        dump($locked_achievements);
 
         return $this->render('achievement/index.html.twig', [
             'controller_name' => 'AchievementController',
             'commander_data' => $commander_data,
-            'unlocked_achievements' => $unlocked_achievements
+            'unlocked_achievements' => $unlocked_achievements,
+            'locked_achievements' => $locked_achievements
         ]);
     }
 }
